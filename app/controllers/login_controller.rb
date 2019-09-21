48d2e90da1a9199877f7b6a6f1a87376e599a54d
arifal <arifalhidayat10@gmail.com>
2019-09-21 23:10:05 +0700

    link,git,routes

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,28 @@
 class LoginController < ApplicationController
   def index
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_params)
+
+    if @user.save
+      redirect_to users_path      
+    else
+      redirect_to login_new_path
+    end
+    
+  end
+
+  private 
+    def set_user
+        @users = User.find(params[:id])
+    end
+
+    def users_params
+        params.require(:user).permit(:username, :password, :alamat)
+    end
 end
